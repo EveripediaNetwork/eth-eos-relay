47f04597cafcd4fe9933d8fa0d94bcab07bdbde1
root <travis@everipedia.com>
2018-01-23 15:11:41 -0800

    More stuff

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # eth-eos-relay
 
 A block header relay supporting the ability to run ETH transactions on EOS
+
+http://www.ethdocs.org/en/latest/ethereum-clients/cpp-ethereum/building-from-source/linux.html#building-linux-from-source
+git clone --recursive https://github.com/ethereum/cpp-ethereum.git
+cd cpp-ethereum
+sudo apt-get install libboost-all-dev libleveldb-dev libcurl4-openssl-dev libmicrohttpd-dev libminiupnpc-dev libgmp-dev
--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
-#include "json.hpp"
-#include "constants.hpp"
+#include "custom-headers/json.hpp"
+#include "custom-headers/constants.hpp"
+#include "custom-headers/radix_tree.hpp"
+#include "libdevcore/SHA3.h"
+#include "libdevcore/rlp.h"
+#include "libdevcore/TrieCommon.h"
 
 using json = nlohmann::json;
 
@@ -28,8 +32,24 @@ bool getTransactionProof (const std::string& txHash){
 
         //  Convert the block data into JSON
         auto txBlockJSON = json::parse(BLOCK_ONE_BLOCK_STRING);
-        std::cout << txBlockJSON.dump(4) << std::endl;
+        // std::cout << txBlockJSON.dump(4) << std::endl;
         std::cout << "\n-----------------------------\n"<< std::endl;
+
+        radix_tree<std::string, int> tree;
+
+        json blockTransactions = txBlockJSON["transactions"];
+
+        for ( json::iterator index = blockTransactions.begin(); index != blockTransactions.end() ; ++index ) {
+                json txJSON = *index;
+                std::cout << txJSON["hash"] << std::endl;
+        }
+
+        // for (auto txID : txBlockJSON["transactions"]){
+        //         auto innerTxJSON = json::parse(txID);
+        //         std::cout << innerTxJSON["hash"] << '\n';
+        // }
+
+
 
 }
 
